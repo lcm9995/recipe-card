1bbf0ff742e487300f82b337e8f21c996596c31a
lcm9995 <lcm9995@nyu.edu>
2025-09-09 08:25:19 -0400

    first commit?

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import RecipeCard from "./components/RecipeCard"
-
-//declare our App component funtion which returns some jsx
 const App = () => {
     return (
-        <>
-            <RecipeCard />
-        </>
+        <><main>
+            <div>App</div>
+        </main></>
     )
-    //if we want to return more than 1 line of code must put it in parentheses 
-    //everything returned needs to just have 1 element wrapping the rest (1 parent )
-    //react fragment = empty opening tag and empty closing tag - use instead of just a random/blank parent/container div if youre just trying to make 1 parent 
 }
-
-
 export default App 
+
+//#whenever we say export default, we don't need curly braces when we import that component 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
-//import react library core
-import React from 'react';
-// react dom allows us to write JSX for web broswers 
-import ReactDOM from 'react-dom/client';
-//to use a component u have to import first 
-import App from './App';
-// ^ when we export default, we import w just the filename 
-import './global.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-  
-//index.html is where all of our compiled react gets injected, and into the div w id root, so this is doing that
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// telling it to render the topmost parent component called App 
-root.render(<App />);
-
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<React.StrictMode>
+    <App/>
+</React.StrictMode>) // this block is the bare minimum to inject react app into an html file
